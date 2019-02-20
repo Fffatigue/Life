a9484d2aa8aa77c9c472d3e0a0559bf03f9fc16e
fffatigue <fffatigue@gmail.com>
2019-02-21 01:26:04 +0700

    Test

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        Field m = new Field(  15,15,3,20);
-        JFrame f=new JFrame();
-        f.add(m);
-        f.setSize(800,800);
+        Field m = new Field( 15, 15, 3, 20 );
+        JFrame f = new JFrame();
+        f.add( m );
+        f.setSize( 800, 800 );
         //f.setLayout(null);
-        f.setVisible(true);
+        f.setVisible( true );
     }
 }
 
