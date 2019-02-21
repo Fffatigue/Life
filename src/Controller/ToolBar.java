e78188ef5164e877dd55cb4a076b47dfb15697e2
Fffatigue <chmil.alexander@gmail.com>
2019-02-21 13:49:58 +0700

    Added icons to toolbar, added statusbar

--- src/Controller/ToolBar.java
+++ src/Controller/ToolBar.java
@@ -3,55 +3,289 @@ package Controller;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class ToolBar extends JToolBar {
     private Field field;
+    private JLabel statusLabel;
 
-    public ToolBar(Field field) {
+    public ToolBar(Field field, JLabel statusLabel) {
+        this.statusLabel = statusLabel;
         this.field = field;
         setFloatable( false );
-        add(new OpenAction());
-        add(new SaveAction());
-        add(new NewAction());
-        add(new JButton( "Xor" ));
-        add(new JButton( "Clear" ));
-        add(new JButton( "Impact" ));
-        add(new JButton( "Next" ));
-        add(new JButton( "Run" ));
+        add( new OpenFileButton() );
+        add( new SaveFileButton() );
+        add( new NewFileButton() );
+        add( new XorButton() );
+        add( new ClearButton() );
+        add( new ImpactButton() );
+        add( new NextButton() );
+        add( new RunButton() );
 
     }
 
-    class OpenAction extends AbstractAction {
-        public OpenAction() {
-            // Настройка иконок
-            putValue(AbstractAction.SMALL_ICON, new ImageIcon("images/open.png"));
+    class OpenFileButton extends JButton implements MouseListener {
+        public OpenFileButton() {
+            setToolTipText( "Open file" );
+            setIcon( new ImageIcon( "src/images/open.png" ) );
+            addMouseListener( this );
         }
-        // Обработка действия
-        public void actionPerformed(ActionEvent e) {
-            // ничего не делаем
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Open file" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
         }
     }
 
-    class SaveAction extends AbstractAction {
-        public SaveAction() {
-            // Настройка иконок
-            putValue(AbstractAction.SMALL_ICON, new ImageIcon("images/save.png"));
+    class SaveFileButton extends JButton implements MouseListener {
+        public SaveFileButton() {
+            setToolTipText( "Save file" );
+            setIcon( new ImageIcon( "src/images/save.png" ) );
+            addMouseListener( this );
         }
-        // Обработка действия
-        public void actionPerformed(ActionEvent e) {
-            // ничего не делаем
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Save file" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
         }
     }
 
-    class NewAction extends AbstractAction {
-        public NewAction() {
-            // Настройка иконок
-            putValue(AbstractAction.SMALL_ICON, new ImageIcon("src/images/new.png"));
+    class NewFileButton extends JButton implements MouseListener {
+        public NewFileButton() {
+            setToolTipText( "Create new file" );
+            setIcon( new ImageIcon( "src/images/new.png" ) );
+            addMouseListener( this );
         }
-        // Обработка действия
-        public void actionPerformed(ActionEvent e) {
-            // ничего не делаем
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Create new file" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
         }
     }
 
+    class XorButton extends JButton implements MouseListener {
+        public XorButton() {
+            setToolTipText( "Enable/Disable XOR mode" );
+            setIcon( new ImageIcon( "src/images/xor.png" ) );
+            addMouseListener( this );
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Enable/Disable XOR mode" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
+        }
+    }
+
+    class ClearButton extends JButton implements MouseListener{
+        public ClearButton() {
+            setToolTipText( "Clear field" );
+            setIcon( new ImageIcon( "src/images/clear.png" ) );
+            addMouseListener( this );
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Clear field" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
+        }
+    }
+
+    class ImpactButton extends JButton implements MouseListener{
+        public ImpactButton() {
+            setToolTipText( "Show/Hide impact" );
+            setIcon( new ImageIcon( "src/images/impact.png" ) );
+            addMouseListener( this );
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Show/hide impact" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
+        }
+    }
+
+    class NextButton extends JButton implements MouseListener{
+        public NextButton() {
+            setToolTipText( "Next step" );
+            setIcon( new ImageIcon( "src/images/next.png" ) );
+            addMouseListener( this );
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Next step" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
+        }
+    }
+
+    class RunButton extends JButton implements MouseListener{
+        public RunButton() {
+            setToolTipText( "Run/Stop" );
+            setIcon( new ImageIcon( "src/images/run.png" ) );
+            addMouseListener( this );
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent mouseEvent) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent mouseEvent) {
+            statusLabel.setText( "Run/Stop" );
+        }
+
+        @Override
+        public void mouseExited(MouseEvent mouseEvent) {
+            statusLabel.setText( "" );
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Controller.Field;
 import Controller.ToolBar;
 
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 import java.awt.*;
 
 public class Main {
@@ -10,12 +11,21 @@ public class Main {
         Field m = new Field( 15, 15, 3, 20 );
         JFrame f = new JFrame();
         f.setLayout( new BorderLayout(  ));
-        f.getContentPane().add( new ToolBar( m ),"North" );
-        f.getContentPane().add( m, "Center" );
+        JPanel statusPanel = new JPanel();
+        statusPanel.setBorder(new BevelBorder( BevelBorder.LOWERED));
+        statusPanel.setPreferredSize(new Dimension(f.getWidth(), 16));
+        statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+        JLabel statusLabel = new JLabel("status");
+        statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        statusPanel.add(statusLabel);
+        f.add( statusLabel, BorderLayout.SOUTH );
+        f.getContentPane().add( new ToolBar( m, statusLabel ),BorderLayout.NORTH );
+        f.getContentPane().add( m, BorderLayout.CENTER );
         //f.pack();
         f.setSize( 800, 800 );
         //f.setLayout(null);
         f.setVisible( true );
+
     }
 }
 
